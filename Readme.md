50d1d21b73890751cd8f32aa75f5db78f056ec12
Jayce Pulsipher <jaycemp@gmail.com>
2013-05-30 01:35:14 -0600

    Working example

---
+++ Readme.md
@@ -0,0 +1,34 @@
+
+# text-dimensions
+
+  Get the width and height of a string of text
+
+## Installation
+
+    $ component install JayceTDE/text-dimensions
+
+## Usage
+
+```javascript
+
+var textDimensions = require('text-dimensions');
+
+textDimensions('Hello world');                                // { width: 71, height: 20 }
+textDimensions('Hello world', { 'font-size': '24px' });       // { width: 113, height: 28 }
+textDimensions('Hello world', document.querySelector('div')); // { width: 126, height: 20 }
+
+```
+
+## Available Styles
+
+[*] font-size
+[*] font-style
+[*] font-weight
+[*] font-family
+[*] line-height
+[*] text-transform
+[*] letter-spacing
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var style = require('computed-style')
+  , el = document.createElement('div')
+  , styleList = [
+      'font-size'
+    , 'font-style'
+    , 'font-weight'
+    , 'font-family'
+    , 'line-height'
+    , 'text-transform'
+    , 'letter-spacing'
+  ]
+;
+
+el.className = 'text-dimensions';
+
+document.body.appendChild(el);
+
+function setStyles(el, styles) {
+    
+    styles = styles || {};
+    
+    var i = 0
+      , l = styleList.length
+    ;
+    while (i < l) {
+        el.style[styleList[i]] = styles[styleList[i]] !== undefined ? styles[styleList[i]] : null;
+        i += 1;
+    }
+}
+
+function extractStyles(el) {
+    var ret = {}
+      , i = 0
+      , l = styleList.length
+    ;
+    while (i < l) {
+        ret[styleList[i]] = style(el)[styleList[i]];
+        i += 1;
+    }
+    return ret;
+}
+
+function dimensions(text, styles) {
+    
+    if (styles instanceof HTMLElement) {
+        styles = extractStyles(styles);
+    }
+    
+    setStyles(el, styles);
+    
+    el.textContent = text;
+    
+    return {
+        width: el.clientWidth
+      , height: el.clientHeight
+    };
+    
+}
+
+module.exports = dimensions;
